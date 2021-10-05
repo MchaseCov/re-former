87b2959bd134983c575edf259aaffa2b544aca99
Chase Covington <mchasecov@gmail.com>
2021-10-04 20:40:20 -0400

    Created 'edit' functionality for users. Moved the user form into a partial. New and Edit user page uses this partial. Added show user page using ID.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum:16 }
+  validates :username, presence: true, length: { minimum: 3, maximum:16 },  uniqueness: { case_sensitive: false }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :password, presence: true, length: { minimum: 6, maximum:128 }
 end
